f39bc768d19af755e36563599b8ee58c51b6d709
leesur2014 <542785042@qq.com>
2017-08-20 10:22:18 -0400

    Cmd to run blender and python script on terminal
    File path may change depend on local path.

---
+++ ImageProcessing.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+blender --background /Users/lili/Desktop/PipeDream/Blender/ImageProcessing.blend  --python /Users/lili/Desktop/PipeDream/Blender/ImageProcessing.py -- /Users/lili/Desktop/peipei_data_2017-08-09-19-57-12
